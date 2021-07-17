cb3c5369fbc8e45883b03577db03e6f04ef2ea15
Alex <esarev@inbox.ru>
2021-07-17 17:17:28 +0700

    slide

--- dist/js/script.js
+++ dist/js/script.js
@@ -215,7 +215,7 @@ window.addEventListener('DOMContentLoaded', () => {
     getResource('http://localhost:3000/menu')
         .then(data => {
             data.forEach(({img, altimg, title, descr, price}) => {
-                new MenuCard(img, altimg, title, descr, price, '.menu . container').render();
+                new MenuCard(img, altimg, title, descr, price, '.menu .container').render();
             });
         });
 
