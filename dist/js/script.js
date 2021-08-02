474d29d2166cc435b0d4dac485f4d6f160d3cfb1
Alex <esarev@inbox.ru>
2021-08-02 14:30:28 +0700

    'up'

--- dist/js/script.js
+++ dist/js/script.js
@@ -406,7 +406,7 @@ window.addEventListener('DOMContentLoaded', () => {
 
     for (let i = 0; i < slides.length; i++) {
         const dot = document.createElement('li');
-        dot.getAttribute('data-slide-to', i + 1);
+        dot.setAttribute('data-slide-to', i + 1);
         dot.style.cssText = `
             box-sizing: content-box;
             flex: 0 1 auto;
@@ -431,7 +431,7 @@ window.addEventListener('DOMContentLoaded', () => {
 
     
     next.addEventListener('click', () => {
-        if (offset == +width.slice(0, width.length - 2) * (slides.length - 1)) {
+        if (offset == (+width.slice(0, width.length - 2) * (slides.length - 1))) {
             offset = 0;
         } else {
             offset += +width.slice(0, width.length - 2); 
@@ -497,7 +497,9 @@ window.addEventListener('DOMContentLoaded', () => {
 
             dots.forEach(dot => dot.style.opacity = '.5');
             dots[slideIndex - 1].style.opacity = 1;
+            
         });
+        
     });
 
     //version 1:
